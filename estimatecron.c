d80decf71d8aac8e64bede1bed536bdd2520bd58
DanCUWA <105371476+DanCUWA@users.noreply.github.com>
2022-08-31 20:15:19 +0800

    Add files via upload

--- estimatecron.c
+++ estimatecron.c
@@ -1,12 +1,28 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
+struct program{
+    char name[40];
+    char type[3];
+    int time;  
+};
+int compare(const void *s1, const void *s2){
+    struct program *p1 = (struct program *) s1; 
+    struct program *p2 = (struct program *) s2; 
+    if (p1->time > p2->time || p1->time == -1){
+        return 1; 
+    } else if (p1->time == p2->time){
+        return 0; 
+    } else { 
+        return -1; 
+    }
+}
 // How do I pass a mutable 3D array 
 int analyse_line(char *line, char *storage_arr[50]){
     if (line[0]!='#'){
         char *split;
         int ind = 0; 
-        split = strtok(line," ");
+        split = strtok(line," \n");
         while (split!=NULL){
             printf("Arr %d: %s\n",ind++,split);
             // storage_arr[*line_num][ind] = *split; 
@@ -16,10 +32,10 @@ int analyse_line(char *line, char *storage_arr[50]){
         printf("Comment\n");
     } 
 }
-int get_day(char cont[], char *days[]){
+int get_el(char cont[], char *days[],int len){
     int ret = -1;
-    for (int i = 0;i<7;i++){
-        printf("day " +i+ " is " + days[i]);
+    for (int i = 0;i<len;i++){
+        printf("day %d is %s" ,i, days[i]);
         if (strcmp(cont,days[i])){
             ret=i;
         }
@@ -36,8 +52,11 @@ int main(int argc, char *argv[]){
     int est_ind = 0; 
     char cron_data[MAX_LINES][6][50];
     int cron_ind = 0; 
-    int defaults[] = {60,24,-1,12,7};
-    char *days[] = {{"sun"},{"mon"},{"tue"},{"wed"},{"thu"},{"fri"},{"sat"}};
+    //Adjust below for days of the week - find 2* and 5mon
+    int defaults[] = {60,24,-1,1,7};
+    char *days[] = {"sun","mon","tue","wed","thu","fri","sat"};
+    char *mons[] = {"jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"};
+    //replace with num weeks 
     int mon_len[] = {31,28,31,30,31,30,31,31,30,31,30,31}; 
     int month = atoi(argv[1]);
     if ( month > 12 || month < 0){
@@ -54,11 +73,11 @@ int main(int argc, char *argv[]){
             if (cur[0]!='#'){
                 char *split;
                 int ind = 0; 
-                split = strtok(cur," ");
+                split = strtok(cur," \n");
                 while (split!=NULL){
                     printf("Arr %d: %s\n",ind,split);
                     strcpy(est_data[est_ind][ind++],split);
-                    split = strtok(NULL," "); 
+                    split = strtok(NULL," \n"); 
                 }
                 est_ind++;
             } else {
@@ -74,16 +93,16 @@ int main(int argc, char *argv[]){
     FILE *cron; 
     if (cron = fopen(argv[2],"r")){
         printf("\nOpening %s:\n",argv[2]);
-        char cur[60]; 
-        while (fgets(cur,60,est)!=NULL){
+        char cur[100]; 
+        while (fgets(cur,100,est)!=NULL){
             if (cur[0]!='#'){
                 char *split;
                 int ind = 0; 
-                split = strtok(cur," ");
+                split = strtok(cur," \n");
                 while (split!=NULL && ind<6){
                     strcpy(cron_data[cron_ind][ind++],split);
                     printf("Arr %d: %s\n",ind-1,cron_data[cron_ind][ind-1]);
-                    split = strtok(NULL," "); 
+                    split = strtok(NULL," \n"); 
                 }                   
                 cron_ind++; 
             } else {
@@ -98,32 +117,33 @@ int main(int argc, char *argv[]){
     printf("PRINTING EST\n");
     for (int i = 0;i<est_ind;i++){
         for (int j = 0;j<2;j++){
-            printf("n%d %s : ",j , est_data[i][j]);
+            printf(" %s ", est_data[i][j]);
         }
         printf("\n");
     }
     printf("PRINTING CRON\n");
     for (int i = 0;i<cron_ind;i++){
         for (int j = 0;j<6;j++){
-            printf("%s : ",cron_data[i][j]);
+            printf(" %s ",cron_data[i][j]);
         }
         printf("\n");
     }
     //FIND MOST COMMON
     int num = 0;
     int max = -1; 
+    struct program exec[40]; 
     char *task;  
     for (int i = 0;i<cron_ind;i++){
         int n=1; 
         if (strcmp(cron_data[i][3],"*")!=0 && atoi(cron_data[i][3]) != month){
-            printf("%d is not the right month for month %d!",atoi(cron_data[i][3])+1,month);
+            printf("%d is not the right month for month %d!",atoi(cron_data[i][3]),month);
             n=0;
-        }
-        for (int j = 0;j<4;j++){
+        } 
+        for (int j = 0;j<5;j++){
             int cur = 1; 
-            if (strcmp(cron_data[i][j],"*")){
+            if (!strcmp(cron_data[i][j],"*")){
                 if (j == 2){
-                    cur = mon_len[month];
+                    cur = 4;//mon_len[month];
                 } else{ 
                     cur = defaults[j]; 
                 }
@@ -135,10 +155,7 @@ int main(int argc, char *argv[]){
             task = cron_data[i][5];   
             max = n;   
         }
-
-    }
-    for (int i=0;i<7;i++){
-        printf("%s is index %d!\n",days[i],get_day(days[i],days));
+        printf("%d %s\n",n,cron_data[i][5]);
     }
     printf("%s occurs the most frequently, for a total of %d times!\n",task,max);
     printf("Overall, there a total of %d processes.\n",num);
