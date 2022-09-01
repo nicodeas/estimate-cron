5a07df20ce57a254a79bdbfe0941d2ee4c956ebf
nicodeas <nicholas@theugroup.co>
2022-09-01 15:13:51 +0800

    add function to process estimates

--- cronestimate.c
+++ cronestimate.c
@@ -3,10 +3,30 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
+// time.h starts counting from 0
+#define CURRENT_YEAR (2022 - 1900 + 1)
 #define NUM_MONTHS 12
+
+#define MAX_MINS 59
+#define MAX_HOURS 23
+#define MAX_HOURS 11
+
 #define MAX_LINES 20
-#define MAX_CHAR_NUM 100
+#define MAX_CHARS 100
+
+#define MAX_NUM_OF_COMMANDS 20
+#define MAX_CONCURRENT_PROCESSES 20
+
+struct command
+{
+    char name[40];
+    int minutes_to_complete;
+};
+
+int total_commands = 0;
+struct command commands[MAX_NUM_OF_COMMANDS];
 
 int process_month(char monthStr[])
 {
@@ -52,6 +72,26 @@ int process_month(char monthStr[])
     }
     return month;
 }
+void process_estimates(char *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    char buffer[MAX_CHARS];
+    if (fp == NULL)
+    {
+        printf("invalid %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+    while (fgets(buffer, MAX_CHARS, fp) != NULL)
+    {
+        if (buffer[0] == '#')
+        {
+            continue;
+        }
+        sscanf(buffer, "%s %i", commands[total_commands].name, &commands[total_commands].minutes_to_complete);
+        printf("The command %s runs for %i minutes\n", commands[total_commands].name, commands[total_commands].minutes_to_complete);
+        total_commands++;
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -67,5 +107,6 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     printf("The month argument ingested is %i\n", month);
-    return 0;
+    process_estimates(argv[3]);
+    exit(EXIT_SUCCESS);
 }
